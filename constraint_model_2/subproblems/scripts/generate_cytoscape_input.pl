0f3e429184216adb803e0273a2664887d74214eb
Kimberly MacKay <kam945@thymine.usask.ca>
2017-11-03 11:59:51 -0600

    Update to generate_cytoscape_input.pl

--- constraint_model_2/subproblems/scripts/generate_cytoscape_input.pl
+++ constraint_model_2/subproblems/scripts/generate_cytoscape_input.pl
@@ -2,10 +2,7 @@
 ## combines the results of each "divide" step and generates the adjacency graph needed for
 ## cytoscape visualization
 ##
-## argument 1: the interaction matrix
 ## argument 2: the number of chromsomes
-## argument 3: the value to scale the interaction frequency by (in order to convert it to
-## an integer
 ## argument 4: the experimental resolution
 ##
 ## Kimberly MacKay July 4, 2016
@@ -17,13 +14,11 @@
 use strict;
 use warnings;
 
-## check to ensure four arguments was passed in
-die "ERROR: must pass in four arguments." if @ARGV != 4;
+## check to ensure two arguments was passed in
+die "ERROR: must pass in two arguments." if @ARGV != 2;
 
-my $HiC_file = $ARGV[0];
-my $num_chr = $ARGV[1];
-my $scale = $ARGV[2]; # for s.pombe work this was set to 1000
-my $experimental_resolution = $ARGV[3];
+my $num_chr = $ARGV[0];
+my $experimental_resolution = $ARGV[1];
 
 # scan each of the results files and collect the result in this hash table
 my %interactions;
@@ -37,7 +32,7 @@ my @stop_index;
 for(my $chr = 1; $chr <= $num_chr; $chr++)
 {
 	print STDERR "What is the ending index of CHR".$chr."? ";
-	my $input = <STDIN>;
+	chomp(my $input = <STDIN>);
 	$stop_index[$chr] = int($input);
 }
 
@@ -55,44 +50,10 @@ for(my $j = 1; $j <= $num_chr; $j++)
 	}
 }
 
-###########################################################################################
-##	Generate the linear interactions
-##		parse the interaction matrix
-###########################################################################################
-## open the interaction matrix file
-open HIC, "$HiC_file" or die "ERROR: $HiC_file could not be opened.";
-chomp(my @matrix_file = <HIC>);
-close HIC;
+my $num_variables = $stop_index[-1];
 
-## $#matrix_file will be the size of the whole-genome contact map since the file also 
-## contains a header line
-my $num_variables = $#matrix_file;
-
-my @frequencies;
-
-## for each line after the header line
-for(my $row = 1; $row <= $#matrix_file; $row++)
-{
-	## split the line
-	my @matrix_line = split /\t/, $matrix_file[$row];
-	
-	## loop through the entire file to extract the frequencies
-	## note: we only have to extract one half of the matrix since it is symmetric
-	## along the diagonal
-	for(my $col = $row; $col <= $num_variables; $col++)
-	{
-		## adjusts NA's to 0's
-		if($matrix_line[$col] =~ "NA")
-		{
-			$frequencies[$row][$col] = 0;
-		}
-		else
-		{
-			# convert the frequency to an integer to improve speed
-			$frequencies[$row][$col] = int($matrix_line[$col]*$scale);
-		}
-	}
-}
+## print the header of the output file
+print "source_node \t target_node \t type_of_interaction \t frequency \t node1_chr \t node2_chr \n";
 
 ###########################################################################################
 ## print out the linear interactions and their "distances" according to the 
@@ -100,18 +61,18 @@ for(my $row = 1; $row <= $#matrix_file; $row++)
 ###########################################################################################
 for(my $row = 1; $row <= $num_variables; $row++)
 {
-	my $col = $row;
 	
-	if($row != $stop_index[1] && $row != $stop_index[2] && $row != $stop_index[3])
+	if($row < $stop_index[1])
 	{
-		if($frequencies[$row][$col] != 0)
-		{
-			print "bin".$row."\tbin".($row+1)."\tlinear\t".(1/$frequencies[$row][$col])."\t".$chrs[$row]."\t".$chrs[($row+1)]."\n";
-		}
-		else
-		{
-			print "bin".$row."\tbin".($row+1)."\tlinear\t".(1/$experimental_resolution)."\t".$chrs[$row]."\t".$chrs[($row+1)]."\n";
-		}
+		print "bin".$row."\tbin".($row+1)."\tlinear1\t".(1/$experimental_resolution)."\t".$chrs[$row]."\t".$chrs[($row+1)]."\n";
+	}
+	elsif($row < $stop_index[2] && $row != $stop_index[1])
+	{
+		print "bin".$row."\tbin".($row+1)."\tlinear2\t".(1/$experimental_resolution)."\t".$chrs[$row]."\t".$chrs[($row+1)]."\n";
+	}
+	elsif($row < $stop_index[3] && $row != $stop_index[1] && $row != $stop_index[2])
+	{
+		print "bin".$row."\tbin".($row+1)."\tlinear3\t".(1/$experimental_resolution)."\t".$chrs[$row]."\t".$chrs[($row+1)]."\n";
 	}
 	
 	# initialize the %dynamics_coefficients hash for each bin to be = 0
@@ -128,48 +89,42 @@ for(my $row = 1; $row <= $num_variables; $row++)
 for(my $chr = 0; $chr < $num_chr; $chr++)
 {
 	# read in the three result files
-	print STDERR "Give the path for the freq file: ";
+	print STDERR "cis-interaction: give the path for the freq file: ";
 	chomp(my $freq_file = <STDIN>);
 	open FREQ_FILE, "$freq_file" or die "ERROR: $freq_file could not be opened";
 	chomp(my @clp_freq_results = <FREQ_FILE>);
 	close FREQ_FILE;
 	
-	print STDERR "Give the path for the row file: ";
+	print STDERR "cis-interaction: give the path for the row file: ";
 	chomp(my $row_file = <STDIN>);
 	open ROW_FILE, "$row_file" or die "ERROR: $row_file could not be opened";
 	chomp(my @clp_row_results = <ROW_FILE>);
 	close ROW_FILE;
 	
-	print STDERR "Give the path for the variable file: ";
-	chomp(my $var_file = <STDIN>);
-	open VAR_FILE, "$var_file" or die "ERROR: $var_file could not be opened";
-	chomp(my @non_zero_vars = <VAR_FILE>);
-	close VAR_FILE;
-	
 	## sanity check to make sure the files are the same length
-	if($#clp_row_results == $#non_zero_vars && $#clp_row_results == $#clp_freq_results)
+	if($#clp_row_results == $#clp_freq_results)
 	{
-		#print STDERR $#non_zero_vars."\n";
-		
-		for(my $i = 0; $i <= $#non_zero_vars; $i++)
+		for(my $i = 0; $i <= $#clp_row_results; $i++)
 		{	
-			## get node1
-			my $node1 = "bin".$non_zero_vars[$i];
-
-			## get node(s)2
-			## split the line from the clp file
-			my @row_results =  split /\s+/, $clp_row_results[$i];
-			
-			## get the corresponding frequency
-			my $freq = $clp_freq_results[$i];
-	
-			# if the interaction could be bound to more then one column
-			for(my $j = 0; $j <= $#row_results; $j++)
+		
+			## if it is a non-zero interaction
+			if($clp_freq_results[$i] != 0)
 			{
-				if($row_results[$j] != 0)
+				## get the value of bin 1 - note it needs to be offest by the chr start index
+				my $node1 = "bin".(($i+1) + $stop_index[$chr]);
+				
+				## get the corresponding frequency
+				my $freq = $clp_freq_results[$i];
+				
+				## get node(s)2
+				## split the line from the clp file
+				my @row_results =  split /\s+/, ($clp_row_results[$i]);
+		
+				# if the interaction could be bound to one or more column(s)
+				for(my $j = 0; $j <= $#row_results; $j++)
 				{
-					my $node2 = "bin".$row_results[$j];
-
+					my $node2 = "bin".($row_results[$j] + $stop_index[$chr]);
+					
 					# add it to the hash
 					$interactions{"cis_".$node1."_".$node2}{NODE1_BIN} = $node1;
 					$interactions{"cis_".$node1."_".$node2}{NODE1_CHR} = ($chr+1);
@@ -181,7 +136,7 @@ for(my $chr = 0; $chr < $num_chr; $chr++)
 					$interactions{"cis_".$node1."_".$node2}{INTERACTION_TYPE} = "cis";
 									
 					$dynamics_coefficents{$node1} = $dynamics_coefficents{$node1} + 1;
-					$dynamics_coefficents{$node2} = $dynamics_coefficents{$node2} + 1;			
+					$dynamics_coefficents{$node2} = $dynamics_coefficents{$node2} + 1;	
 				}
 			}
 		}
@@ -192,76 +147,56 @@ for(my $chr = 0; $chr < $num_chr; $chr++)
 	}
 }
 
+# holds the stop index that needs to be added to the second chromosomes bin number
+my @temp = [558, 558, 1012];
+
 # for each inter-interaction
 for(my $chr = 0; $chr < $num_chr; $chr++)
 {
-	# read in the three result files
-	print STDERR "Give the path for the freq file: ";
+	# read in the two result files
+	print STDERR "trans-interaction: give the path for the freq file: ";
 	chomp(my $freq_file = <STDIN>);
 	open FREQ_FILE, "$freq_file" or die "ERROR: $freq_file could not be opened";
 	chomp(my @clp_freq_results = <FREQ_FILE>);
 	close FREQ_FILE;
 		
-	print STDERR "Give the path for the row file: ";
+	print STDERR "trans-interaction: give the path for the row file: ";
 	chomp(my $row_file = <STDIN>);
 	open ROW_FILE, "$row_file" or die "ERROR: $row_file could not be opened";
 	chomp(my @clp_row_results = <ROW_FILE>);
 	close ROW_FILE;
 	
-	print STDERR "Give the path for the variable file: ";
-	chomp(my $var_file = <STDIN>);
-	open VAR_FILE, "$var_file" or die "ERROR: $var_file could not be opened";
-	chomp(my @non_zero_vars = <VAR_FILE>);
-	close VAR_FILE;
+	# get the first chromosome of the trans interaction
+	print STDERR "What is the first chormosome? ";
+	chomp(my $chr1 = <STDIN>);
+	
+	# get the second chromosome of the trans interaction
+	print STDERR "What is the second chormosome? ";
+	chomp(my $chr2 = <STDIN>);
 	
 	## sanity check to make sure the files are the same length
-	if($#clp_row_results == $#non_zero_vars && $#clp_row_results == $#clp_freq_results)
+	if($#clp_row_results == $#clp_freq_results)
 	{
-		for(my $i = 0; $i <= $#non_zero_vars; $i++)
+		for(my $i = 0; $i <= $#clp_row_results; $i++)
 		{	
-			## get node1
-			my $node1 = "bin".$non_zero_vars[$i];
-
-			## get the chromosome that corresponds to node 1
-			my $chr1 = 0;
-			
-			my $found = 0;
-			
-			for(my $k = 1; $k <= $num_chr && !$found; $k++)
+			## if it is a non-zero interaction
+			if($clp_freq_results[$i] != 0)
 			{
-				if($non_zero_vars[$i] <= $stop_index[$k])
+				## get the value of bin 1 - note it needs to be offest by the chr start index
+				my $node1 = "bin".(($i+1) + $stop_index[$chr1-1]);
+				
+				## get the corresponding frequency
+				my $freq = $clp_freq_results[$i];
+				
+				## get node(s)2
+				## split the line from the clp file
+				my @row_results =  split /\s+/, $clp_row_results[$i];
+				
+				# if the interaction could be bound to one or more column(s)
+				for(my $j = 0; $j <= $#row_results; $j++)
 				{
-					$chr1 = $k;
-					$found = 1;
-				}
-			}
-
-			## get node(s)2
-			## split the line from the clp file
-			my @row_results =  split /\s+/, $clp_row_results[$i];
-			
-			## get the corresponding frequency
-			my $freq = $clp_freq_results[$i];
-	
-			# if the interaction could be bound to more then one column
-			for(my $j = 0; $j <= $#row_results; $j++)
-			{
-				if($row_results[$j] != 0)
-				{
-					my $node2 = "bin".$row_results[$j];
+					my $node2 = "bin".($row_results[$j] + $stop_index[$chr2-1]);
 					
-					## get the chromosome that corresponds to node 1
-					my $chr2;
-					$found = 0;
-					for(my $l = 1; $l <= $num_chr && !$found; $l++)
-					{
-						if($row_results[$j] <= $stop_index[$l])
-						{
-							$chr2 = $l;
-							$found = 1;
-						}
-					}
-
 					# add it to the hash
 					$interactions{"trans_".$node1."_".$node2}{NODE1_BIN} = $node1;
 					$interactions{"trans_".$node1."_".$node2}{NODE1_CHR} = $chr1;
@@ -273,7 +208,7 @@ for(my $chr = 0; $chr < $num_chr; $chr++)
 					$interactions{"trans_".$node1."_".$node2}{INTERACTION_TYPE} = "trans";
 									
 					$dynamics_coefficents{$node1} = $dynamics_coefficents{$node1} + 1;
-					$dynamics_coefficents{$node2} = $dynamics_coefficents{$node2} + 1;			
+					$dynamics_coefficents{$node2} = $dynamics_coefficents{$node2} + 1;
 				}
 			}
 		}
